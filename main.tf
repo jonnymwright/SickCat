0025f0f507666e191634dce5916b32d7a58729d0
Jonny Wright <jmwright@scottlogic.co.uk>
2019-02-12 16:09:45 +0000

    Let running user define key file and set name on command line

--- main.tf
+++ main.tf
@@ -1,110 +1,112 @@
 provider "aws" {
   region = "eu-west-2"
 }
-variable "privateKeyLocation" {
+variable "privateKey" {
   type = "string"
 }
 
+# EC2 instance to build the scanner
 resource "aws_instance" "binaries_builder_instance" {
   # Hard coded to Amazon Linux 2018
-  ami           = "ami-01419b804382064e4"
-  instance_type = "t2.micro"
-  key_name      = "JonnyKey"
+  ami             = "ami-01419b804382064e4"
+  instance_type   = "t2.micro"
+  key_name        = "${var.privateKey}"
+  security_groups = ["${aws_security_group.ssh_access.name}"]
 
   tags = {
     Name        = "KittyCat builder"
     Description = "Virus scan binaries builder"
   }
 
-  security_groups = ["${aws_security_group.ssh_access.name}"]
+  # Create files and folders on EC2 instance that are needed to build the zip
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir config",
+      "mkdir src",
+      "mkdir etc",
+    ]
 
-    provisioner "remote-exec" {
-      inline = [
-        "mkdir config",
-        "mkdir src",
-        "mkdir etc",
-      ]
-
-      connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file(var.privateKeyLocation)}"
-      }
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.privateKey)}"
     }
+  }
 
-    provisioner "file" {
-      source      = "./build.sh"
-      destination = "~/build.sh"
+  provisioner "file" {
+    source      = "./build.sh"
+    destination = "~/build.sh"
 
-      connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file(var.privateKeyLocation)}"
-      }
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.privateKey)}"
     }
+  }
 
-    provisioner "file" {
-      source      = "./config/local.yaml"
-      destination = "~/config/local.yaml"
+  provisioner "file" {
+    source      = "./config/local.yaml"
+    destination = "~/config/local.yaml"
 
-      connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file(var.privateKeyLocation)}"
-      }
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.privateKey)}"
     }
-    
-    provisioner "file" {
-      source      = "./etc/freshclam.conf"
-      destination = "~/etc/freshclam.conf"
+  }
 
-      connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file(var.privateKeyLocation)}"
-      }
-    }
-    
-    provisioner "file" {
-      source      = "./dist/handler.js"
-      destination = "~/src/handler.js"
+  provisioner "file" {
+    source      = "./etc/freshclam.conf"
+    destination = "~/etc/freshclam.conf"
 
-      connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file(var.privateKeyLocation)}"
-      }
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.privateKey)}"
     }
-    
-    provisioner "file" {
-      source      = "./dist/update.js"
-      destination = "~/src/update.js"
+  }
 
-      connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file(var.privateKeyLocation)}"
-      }
-    }
-    
-    provisioner "remote-exec" {
-      inline = [
-        "cd ~",
-        "sudo chmod 777 build.sh",
-        "sudo sh build.sh"
-      ]
+  provisioner "file" {
+    source      = "./dist/handler.js"
+    destination = "~/src/handler.js"
 
-      connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = "${file(var.privateKeyLocation)}"
-      }
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.privateKey)}"
     }
+  }
+
+  provisioner "file" {
+    source      = "./dist/update.js"
+    destination = "~/src/update.js"
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.privateKey)}"
+    }
+  }
+
+  # Call a script to build the scanner
+  provisioner "remote-exec" {
+    inline = [
+      "cd ~",
+      "sudo chmod 777 build.sh",
+      "sudo sh build.sh"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = "${file(var.privateKey)}"
+    }
+  }
 }
 
-# Allow ssh into the instance
+# Allow ssh into and wget out of the instance
 resource "aws_security_group" "ssh_access" {
-  description = "allow remote ssh into ec2 instance"
+  description = "Allow ssh into and wget out of the instance"
 
   ingress {
     from_port   = 22
@@ -118,13 +120,5 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 0
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Get the zip that was created
-resource "null_resource" "FTP_get_zip" {
-  provisioner "local-exec" {
-    command = "sftp -i '${var.privateKeyLocation}' -o StrictHostKeyChecking=accept-new ec2-user@${aws_instance.binaries_builder_instance.public_ip}:/home/ec2-user/scan.zip . "
-    interpreter = ["PowerShell", "-Command"]
   }
 }
